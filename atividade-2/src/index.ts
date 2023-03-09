2bfc32c9303663946fa964db83e7cc813bb1b664
GuilhermeSGodoy <gui.santos.godoy@gmail.com>
2023-03-09 12:10:03 -0300

    atualizando arquivos

--- atividade-2/src/index.ts
+++
@@ -1,7 +0,0 @@
-/* eslint no-console: ["error", { allow: ["log"] }] */
-import app from './app'
-import { logger } from './config'
-
-app.listen('3000', () => {
-  logger.info('[app] service up')
-})
